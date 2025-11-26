3c213a07628ff235839c4d60092889bb8c6ae21a
Froxayn123 <rizkymyusuf123@gmail.com>
2025-11-26 15:17:03 +0700

    feat: Adding Room Detail Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import AllRooms from "./pages/AllRooms";
+import RoomDetails from "./pages/RoomDetails";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/rooms" element={<AllRooms />} />
+      <Route path="/rooms/:id" element={<RoomDetails />} />
     </Routes>
   );
 };
