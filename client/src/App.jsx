72c68cda6a3fadf4f594389490d1f2a83d9d9a72
Froxayn123 <rizkymyusuf123@gmail.com>
2025-11-26 15:49:16 +0700

    feat: Adding My Bookings Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import AllRooms from "./pages/AllRooms";
 import RoomDetails from "./pages/RoomDetails";
+import MyBookings from "./pages/MyBookings";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Route path="/" element={<Home />} />
       <Route path="/rooms" element={<AllRooms />} />
       <Route path="/rooms/:id" element={<RoomDetails />} />
+      <Route path="/my-bookings" element={<MyBookings />} />
     </Routes>
   );
 };
