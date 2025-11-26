2dd80b79a4b8918643e0e312cbd1fce9e03e5360
Froxayn123 <rizkymyusuf123@gmail.com>
2025-11-26 13:21:09 +0700

    feat: Adding Rooms Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import AllRooms from "./pages/AllRooms";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/rooms" element={<AllRooms />} />
     </Routes>
   );
 };
