a9f4e46aabe2361e2665bc4e567f8d5493b867e6
Froxayn123 <rizkymyusuf123@gmail.com>
2025-11-27 12:56:42 +0700

    feat: Adding HotelReg component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,15 +3,28 @@ import Home from "./pages/Home";
 import AllRooms from "./pages/AllRooms";
 import RoomDetails from "./pages/RoomDetails";
 import MyBookings from "./pages/MyBookings";
+import HotelReg from "./components/HotelReg";
+import Layout from "./pages/hotelOwner/Layout";
+import Dashboard from "./pages/hotelOwner/Dashboard";
+import AddRoom from "./pages/hotelOwner/AddRoom";
+import ListRoom from "./pages/hotelOwner/ListRoom";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/rooms" element={<AllRooms />} />
-      <Route path="/rooms/:id" element={<RoomDetails />} />
-      <Route path="/my-bookings" element={<MyBookings />} />
-    </Routes>
+    <>
+      {false && <HotelReg />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/rooms" element={<AllRooms />} />
+        <Route path="/rooms/:id" element={<RoomDetails />} />
+        <Route path="/my-bookings" element={<MyBookings />} />
+        <Route path="/owner" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="add-room" element={<AddRoom />} />
+          <Route path="list-room" element={<ListRoom />} />
+        </Route>
+      </Routes>
+    </>
   );
 };
 
